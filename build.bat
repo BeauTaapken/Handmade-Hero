e9c67858f83d7dae2d94f6fbf653561d77f276c3
Beau <beau@lioncode.nl>
2024-05-06 20:07:17 +0200

    Window is now being drawn, and while resizing it switches between black and white colors

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 mkdir -p ./build
 pushd ./build
-x86_64-w64-mingw32-gcc -g ../code/win32_handmade.cpp -o handmade-windows
+x86_64-w64-mingw32-gcc -g ../code/win32_handmade.cpp -o handmade-windows -lgdi32
 popd
--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,12 +1,107 @@
 #include <windows.h>
 
+LRESULT CALLBACK MainWindowCallback(HWND Window,
+                                    UINT Message,
+                                    WPARAM WParam,
+                                    LPARAM LParam) {
+    LRESULT Result = 0;
+
+    switch(Message) {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n");
+        } break;
+
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            static DWORD Operation = WHITENESS;
+            PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+            if (Operation == WHITENESS) {
+                Operation = BLACKNESS;
+            } else {
+                Operation = WHITENESS;
+            }
+            EndPaint(Window, &Paint);
+        } break;
+
+        default:
+        {
+//                OutputDebugStringA("default\n");
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        } break;
+    }
+
+    return(Result);
+}
+
 int CALLBACK WinMain(
-    HINSTANCE hInstance,
-    HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine,
-    int nShowCmd
+    HINSTANCE Instance,
+    HINSTANCE PrevInstance,
+    LPSTR CmdLine,
+    int ShowCmd
 ) {
-    MessageBox(0, "This is Handmade Hero.", "Handmade Hero", MB_OK|MB_ICONINFORMATION);
+    WNDCLASS WindowClass = {};
+
+    // TODO(beau): Check if HREDRAW/VREDRAW/OWNDC still matter
+    WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = Instance;
+//    WindowClass.hIcon = ;
+    WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+    if (RegisterClass(&WindowClass)) {
+        HWND WindowHandle = CreateWindowEx(
+            0,
+            WindowClass.lpszClassName,
+            "Handmade Hero",
+            WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            0,
+            0,
+            Instance,
+            0
+        );
+        if (WindowHandle) {
+            for(;;) {
+                MSG Message;
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if (MessageResult > 0) {
+                    TranslateMessage(&Message);
+                    DispatchMessage(&Message);
+                } else {
+                    break;
+                }
+            }
+        } else {
+            // TODO(beau): Logging
+        }
+    } else {
+        // TODO(beau): Logging
+    }
 
     return(0);
 }
