5ca1eadd75a6f5e173572c773626641214e0f5da
Beau <beau@lioncode.nl>
2024-06-01 00:38:39 +0200

    Added Xinput and setup for Dsound

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -6,102 +6,285 @@
    $Notice: (C) Copyright 2024 by Beau Taapken. All Rights Reserved. $
    =================================================================== */
 
+#include <cstdint>
 #include <windows.h>
+#include <stdint.h>
+#include <xinput.h>
+#include <dsound.h>
 
 #define internal static
 #define local_persist static
 #define global_variable static
 
+struct win32_offscreen_buffer {
+    // NOTE(beau): Pixels are always 32-bits wide, Memory Order  0x BB GG RR xx
+    BITMAPINFO Info;
+    void *Memory;
+    int Width;
+    int Height;
+    int Pitch;
+};
+
+struct win32_window_dimension {
+    int Width;
+    int Height;
+};
+
 // TODO(beau): This is a global for now.
-global_variable bool Running;
+global_variable bool GlobalRunning;
+global_variable win32_offscreen_buffer GlobalBackbuffer;
 
-global_variable BITMAPINFO BitmapInfo;
-global_variable void *BitmapMemory;
-global_variable HBITMAP BitmapHandle;
-global_variable HDC BitmapDeviceContext;
+// NOTE(beau): XInputGetState
+#define X_INPUT_GET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_STATE *pState)
+typedef X_INPUT_GET_STATE(x_input_get_state);
+X_INPUT_GET_STATE(XInputGetStateStub) {
+    return(ERROR_DEVICE_NOT_CONNECTED);
+}
+global_variable x_input_get_state *XInputGetState_ = XInputGetStateStub;
+#define XInputGetState XInputGetState_
 
-internal void Win32ResizeDIBSection(int Width, int Height) {
+// NOTE(beau): XInputSetState
+#define X_INPUT_SET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_VIBRATION *pVibration)
+typedef X_INPUT_SET_STATE(x_input_set_state);
+X_INPUT_SET_STATE(XInputSetStateStub) {
+    return(ERROR_DEVICE_NOT_CONNECTED);
+}
+global_variable x_input_set_state *XInputSetState_ = XInputSetStateStub;
+#define XInputSetState XInputSetState_
+
+#define DIRECT_SOUND_CREATE(name) HRESULT WINAPI name(LPCGUID pcGuidDevice, LPDIRECTSOUND *PPds, LPUNKNOWN pUnkOuter)
+typedef DIRECT_SOUND_CREATE(direct_sound_create);
+
+internal void Win32LoadXInput(void) {
+    // TODO(beau): Test this on Windows 8
+    HMODULE XInputLibrary = LoadLibraryA("xinput1_4.dll");
+    if (!XInputLibrary) {
+        // TODO(beau): Diagnostic
+        XInputLibrary = LoadLibraryA("xinput1_3.dll");
+    }
+
+    if (XInputLibrary) {
+        XInputGetState = (x_input_get_state *)GetProcAddress(XInputLibrary, "XInputGetState");
+        if (!XInputGetState) {XInputGetState = XInputGetStateStub;}
+        XInputSetState = (x_input_set_state *)GetProcAddress(XInputLibrary, "XInputSetState");
+        if (!XInputGetState) {XInputSetState = XInputSetStateStub;}
+
+        // TODO(beau): Diagnostic
+    } else {
+        // TODO(beau): Diagnostic
+    }
+}
+
+internal void Win32InitDSound(HWND Window, int32_t SamplesPerSecond, int32_t BufferSize) {
+    // NOTE(beau): Load the library
+    HMODULE DSoundLibrary = LoadLibraryA("dsound.dll");
+
+    if (DSoundLibrary) {
+        // NOTE(beau): Get a DirectSound object! - cooperative
+        direct_sound_create *DirectSoundCreate = (direct_sound_create *)GetProcAddress(DSoundLibrary, "DirectSoundCreate");
+
+        // TODO(beau): Double-check that this works on XP - DirectSound8 or 7??
+        LPDIRECTSOUND DirectSound;
+        if (DirectSoundCreate && SUCCEEDED(DirectSoundCreate(0, &DirectSound, 0))) {
+            WAVEFORMATEX WaveFormat = {};
+            WaveFormat.wFormatTag = WAVE_FORMAT_PCM;
+            WaveFormat.nChannels = 2;
+            WaveFormat.nSamplesPerSec = SamplesPerSecond;
+            WaveFormat.nBlockAlign = (WaveFormat.nChannels * WaveFormat.wBitsPerSample) / 8;
+            WaveFormat.nAvgBytesPerSec = WaveFormat.nSamplesPerSec * WaveFormat.nBlockAlign;
+            WaveFormat.wBitsPerSample = 16;
+            WaveFormat.cbSize = 0;
+
+            if(!SUCCEEDED(DirectSound->SetCooperativeLevel(Window, DSSCL_PRIORITY))) {
+                DSBUFFERDESC BufferDescription = {};
+                BufferDescription.dwSize = sizeof(BufferDescription);
+                BufferDescription.dwFlags = DSBCAPS_PRIMARYBUFFER;
+
+                // NOTE(beau): "Create" a primary buffer
+                // TODO(beau): DSBCAPS_GLOBALFOCUS?
+                LPDIRECTSOUNDBUFFER PrimaryBuffer;
+                if (SUCCEEDED(DirectSound->CreateSoundBuffer(&BufferDescription, &PrimaryBuffer, 0))) {
+                    HRESULT Error = PrimaryBuffer->SetFormat(&WaveFormat);
+                    if (SUCCEEDED(Error)) {
+                        // NOTE(beau): we have finally set the format!
+                        OutputDebugStringA("Primary buffer format was set.\n");
+                    } else {
+                        // TODO(beau): Diagnostic
+                    }
+                }
+            } else {
+                // TODO(beau): Diagnostic
+            }
+
+            // NOTE(beau): "Create" a secondary buffer
+            // TODO(beau): DSBCAPS_GETCURRENTPOSITION2
+            DSBUFFERDESC BufferDescription = {};
+            BufferDescription.dwSize = sizeof(BufferDescription);
+            BufferDescription.dwFlags = 0;
+            BufferDescription.dwBufferBytes = BufferSize;
+            BufferDescription.lpwfxFormat = &WaveFormat;
+            LPDIRECTSOUNDBUFFER SecondaryBuffer;
+            HRESULT Error = DirectSound->CreateSoundBuffer(&BufferDescription, &SecondaryBuffer, 0);
+            if (SUCCEEDED(Error)) {
+                OutputDebugStringA("Secondary buffer created successfully.\n");
+            }
+        } else {
+            // TODO(beau): Diagnostic
+        }
+    } else {
+        // TODO(beau): Diagnostic
+    }
+}
+
+internal win32_window_dimension Win32GetWindowDimension(HWND Window) {
+    win32_window_dimension Result;
+
+    RECT ClientRect;
+    GetClientRect(Window, &ClientRect);
+    Result.Width = ClientRect.right - ClientRect.left;
+    Result.Height = ClientRect.bottom - ClientRect.top;
+
+    return(Result);
+}
+
+internal void RenderWeirdGradient(win32_offscreen_buffer *Buffer, int BlueOffset, int GreenOffset){
+    // TODO(beau): Let's see what the optimizer does
+    uint8_t *Row = (uint8_t *)Buffer->Memory;
+    for(int Y = 0; Y < Buffer->Height; Y++) {
+        uint32_t *Pixel = (uint32_t *)Row;
+        for(int X = 0; X < Buffer->Width; X++) {
+            uint8_t Blue = (X + BlueOffset);
+            uint8_t Green = (Y + GreenOffset);
+
+            *Pixel++ = ((Green << 8) | Blue);
+        }
+
+        Row += Buffer->Pitch;
+    }
+}
+
+internal void Win32ResizeDIBSection(win32_offscreen_buffer *Buffer, int Width, int Height) {
     // TODO(beau): Bulletproof this.
     // Maybe don't free first, free after, then free first if that fails.
 
-    if (BitmapHandle) {
-        DeleteObject(BitmapHandle);
-    }
-    if (!BitmapDeviceContext) {
-        // TODO(beau): Should we recreate these under certain special circumstances
-        BitmapDeviceContext = CreateCompatibleDC(0);
+    if (Buffer->Memory) {
+        VirtualFree(Buffer->Memory, 0, MEM_RELEASE);
     }
 
-    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-    BitmapInfo.bmiHeader.biWidth = Width;
-    BitmapInfo.bmiHeader.biHeight = Height;
-    BitmapInfo.bmiHeader.biPlanes = 1;
-    BitmapInfo.bmiHeader.biBitCount = 32;
-    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+    Buffer->Width = Width;
+    Buffer->Height = Height;
+    int BytesPerPixel = 4;
 
-    BitmapHandle = CreateDIBSection(
-        BitmapDeviceContext, &BitmapInfo,
-        DIB_RGB_COLORS,
-        &BitmapMemory,
-        0, 0);
+    Buffer->Info.bmiHeader.biSize = sizeof(Buffer->Info.bmiHeader);
+    Buffer->Info.bmiHeader.biWidth = Buffer->Width;
+    Buffer->Info.bmiHeader.biHeight = -Buffer->Height;
+    Buffer->Info.bmiHeader.biPlanes = 1;
+    Buffer->Info.bmiHeader.biBitCount = 32;
+    Buffer->Info.bmiHeader.biCompression = BI_RGB;
+
+    // NOTE(beau): Thank you to Chris Hecker of Spy Party fame
+    // for clarifying the deal with StretchDIBits and BitBlt!
+    // No more DC for us.
+    int BitmapMemorySize = (Buffer->Width * Buffer->Height) * BytesPerPixel;
+    Buffer->Memory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+
+    Buffer->Pitch = Width * BytesPerPixel;
+
+    // TODO(beau): Probably clear this to black;
+
 }
 
-internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height) {
+internal void Win32DisplayBufferInWindow(win32_offscreen_buffer *Buffer, HDC DeviceContext,
+                                         int WindowWidth, int WindowHeight) {
+    // TODO(beau): Aspect ratio correction
+    // TODO(beau): Play with stretch modes
     StretchDIBits(DeviceContext,
-                  X, Y, Width, Height,
-                  X, Y, Width, Height,
-                  BitmapMemory,
-                  &BitmapInfo,
+//                  X, Y, Width, Height,
+//                  X, Y, Width, Height,
+                  0, 0, WindowWidth, WindowHeight,
+                  0, 0, Buffer->Width, Buffer->Height,
+                  Buffer->Memory,
+                  &Buffer->Info,
                   DIB_RGB_COLORS, SRCCOPY);
 }
 
-LRESULT CALLBACK Win32MainWindowCallback(HWND Window,
+internal LRESULT CALLBACK Win32MainWindowCallback(HWND Window,
                                          UINT Message,
                                          WPARAM WParam,
                                          LPARAM LParam) {
     LRESULT Result = 0;
 
     switch(Message) {
-        case WM_SIZE:
-        {
-            RECT ClientRect;
-            GetClientRect(Window, &ClientRect);
-            int Width = ClientRect.right - ClientRect.left;
-            int Height = ClientRect.bottom - ClientRect.top;
-            Win32ResizeDIBSection(Width, Height);
+        case WM_SIZE: {
         } break;
 
-        case WM_CLOSE:
-        {
+        case WM_CLOSE: {
             // TODO(beau): Handle this with a message to the user?
-            Running = false;
+            GlobalRunning = false;
         } break;
 
-        case WM_ACTIVATEAPP:
-        {
+        case WM_ACTIVATEAPP: {
             OutputDebugStringA("WM_ACTIVATEAPP\n");
         } break;
 
-        case WM_DESTROY:
-        {
+        case WM_DESTROY: {
             // TODO(beau): Handle this as an error - recreate window?
-            Running = false;
+            GlobalRunning = false;
         } break;
 
-        case WM_PAINT:
-        {
+        case WM_SYSKEYDOWN:
+        case WM_SYSKEYUP:
+        case WM_KEYDOWN:
+        case WM_KEYUP: {
+            uint32_t VKCode = WParam;
+            bool WasDown = ((LParam & (1 << 30)) != 0);
+            bool IsDown = ((LParam & (1 << 31)) == 0);
+
+            if (WasDown != IsDown) {
+                if (VKCode == 'W') {
+                }
+                else if (VKCode == 'A') {
+                }
+                else if (VKCode == 'S') {
+                }
+                else if (VKCode == 'D') {
+                }
+                else if (VKCode == 'Q') {
+                }
+                else if (VKCode == 'E') {
+                }
+                else if (VKCode == VK_UP) {
+                }
+                else if (VKCode == VK_LEFT) {
+                }
+                else if (VKCode == VK_DOWN) {
+                }
+                else if (VKCode == VK_RIGHT) {
+                }
+                else if (VKCode == VK_ESCAPE) {
+                }
+                else if (VKCode == VK_SPACE) {
+                }
+            }
+
+            // This should be 0 or 1, if not, it'll still work, so no bool.
+            int32_t AltKeyWasDown = (LParam & (1 << 29));
+            if ((VKCode == VK_F4) && AltKeyWasDown) {
+                GlobalRunning = false;
+            }
+        } break;
+
+        case WM_PAINT: {
             PAINTSTRUCT Paint;
             HDC DeviceContext = BeginPaint(Window, &Paint);
-            int X = Paint.rcPaint.left;
-            int Y = Paint.rcPaint.top;
-            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-            Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+
+            win32_window_dimension Dimension = Win32GetWindowDimension(Window);
+            Win32DisplayBufferInWindow(&GlobalBackbuffer, DeviceContext, Dimension.Width, Dimension.Height);
+
             EndPaint(Window, &Paint);
         } break;
 
-        default:
-        {
+        default: {
 //                OutputDebugStringA("default\n");
             Result = DefWindowProc(Window, Message, WParam, LParam);
         } break;
@@ -116,17 +299,20 @@ int CALLBACK WinMain(
     LPSTR CmdLine,
     int ShowCmd
 ) {
-    WNDCLASS WindowClass = {};
+    Win32LoadXInput();
 
-    // TODO(beau): Check if HREDRAW/VREDRAW/OWNDC still matter
-    WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+    WNDCLASSA WindowClass = {};
+
+    Win32ResizeDIBSection(&GlobalBackbuffer, 1280, 720);
+
+    WindowClass.style = CS_HREDRAW|CS_VREDRAW|CS_OWNDC;
     WindowClass.lpfnWndProc = Win32MainWindowCallback;
     WindowClass.hInstance = Instance;
 //    WindowClass.hIcon = ;
     WindowClass.lpszClassName = "HandmadeHeroWindowClass";
 
     if (RegisterClass(&WindowClass)) {
-        HWND WindowHandle = CreateWindowEx(
+        HWND Window = CreateWindowEx(
             0,
             WindowClass.lpszClassName,
             "Handmade Hero",
@@ -140,17 +326,70 @@ int CALLBACK WinMain(
             Instance,
             0
         );
-        if (WindowHandle) {
-            Running = true;
-            while(Running) {
+        if (Window) {
+            // NOTE(beau): Since we specified CS_OWNDC, we can just get one device context and use it forever because we are not sharing it with anyone.
+            HDC DeviceContext = GetDC(Window);
+
+            int XOffset = 0;
+            int YOffset = 0;
+
+            Win32InitDSound(Window, 48000, 48000*sizeof(int16_t)*2);
+
+            GlobalRunning = true;
+            while(GlobalRunning) {
                 MSG Message;
-                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
-                if (MessageResult > 0) {
+                while (PeekMessage(&Message, 0, 0, 0, PM_REMOVE)) {
+                    if (Message.message == WM_QUIT) {
+                        GlobalRunning = false;
+                    }
+
                     TranslateMessage(&Message);
                     DispatchMessage(&Message);
-                } else {
-                    break;
                 }
+
+                // TODO(beau): Should we poll this more frequently
+                for (DWORD ControllerIndex=0; ControllerIndex < XUSER_MAX_COUNT; ControllerIndex++) {
+                    XINPUT_STATE ControllerState;
+                    if (XInputGetState(ControllerIndex, &ControllerState) == ERROR_SUCCESS) {
+                        // NOTE(beau): This controller is plugged in
+                        // TODO(beau): See if ControllerState.dwPacketNumber increments too rapidly.
+                        XINPUT_GAMEPAD *Pad = &ControllerState.Gamepad;
+
+                        bool Up = (Pad->wButtons & XINPUT_GAMEPAD_DPAD_UP);
+                        bool Down = (Pad->wButtons & XINPUT_GAMEPAD_DPAD_DOWN);
+                        bool Left = (Pad->wButtons & XINPUT_GAMEPAD_DPAD_LEFT);
+                        bool Right = (Pad->wButtons & XINPUT_GAMEPAD_DPAD_RIGHT);
+                        bool Start = (Pad->wButtons & XINPUT_GAMEPAD_START);
+                        bool Back = (Pad->wButtons & XINPUT_GAMEPAD_BACK);
+                        bool LeftShoulder = (Pad->wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER);
+                        bool RightShoulder = (Pad->wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER);
+                        bool AButton = (Pad->wButtons & XINPUT_GAMEPAD_A);
+                        bool BButton = (Pad->wButtons & XINPUT_GAMEPAD_B);
+                        bool XButton = (Pad->wButtons & XINPUT_GAMEPAD_X);
+                        bool YButton = (Pad->wButtons & XINPUT_GAMEPAD_Y);
+
+                        int16_t StickX = Pad->sThumbLX;
+                        int16_t StickY = Pad->sThumbLY;
+
+                        if (AButton) {
+                            YOffset += 2;
+                        }
+                    } else {
+                        // NOTE(beau): The controller is not available
+                    }
+                }
+
+                XINPUT_VIBRATION Vibration;
+                Vibration.wLeftMotorSpeed = 60000;
+                Vibration.wRightMotorSpeed = 60000;
+                XInputSetState(0, &Vibration);
+
+                RenderWeirdGradient(&GlobalBackbuffer, XOffset, YOffset);
+
+                win32_window_dimension Dimension = Win32GetWindowDimension(Window);
+                Win32DisplayBufferInWindow(&GlobalBackbuffer, DeviceContext, Dimension.Width, Dimension.Height);
+
+                ++XOffset;
             }
         } else {
             // TODO(beau): Logging
